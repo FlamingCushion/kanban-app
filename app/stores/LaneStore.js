b4d1bffadd7c9866ffb0f38456fc3ac7ca42e1d9
Martyn Gosling <flamingcushion@gmail.com>
2017-03-07 19:28:07 +0000

    complete DnD kanban/notes demo

--- app/stores/LaneStore.js
+++ app/stores/LaneStore.js
@@ -1,19 +1,32 @@
+import update from 'react-addons-update';
 import LaneActions from '../actions/LaneActions';
 
 export default class LaneStore {
     constructor() {
         this.bindActions(LaneActions);
 
-        this.Lanes = [];
+        this.lanes = [];
     }
     create(lane) {
-        //lane.notes = lane.notes || [];
+        // If `notes` aren't provided for some reason,
+        // default to an empty array.
+        lane.notes = lane.notes || [];
 
         this.setState({
             lanes: this.lanes.concat(lane)
         });
     }
+    update(updatedLane) {
+        this.setState({
+            lanes: this.lanes.map(lane => {
+                if(lane.id === updatedLane.id) {
+                    return Object.assign({}, lane, updatedLane);
+                }
 
+                return lane;
+            })
+        })
+    }
     attachToLane({laneId, noteId}) {
         this.setState({
             lanes: this.lanes.map(lane => {
@@ -29,7 +42,6 @@ export default class LaneStore {
             })
         });
     }
-
     detachFromLane({laneId, noteId}) {
         this.setState({
             lanes: this.lanes.map(lane => {
@@ -41,4 +53,30 @@ export default class LaneStore {
             })
         });
     }
+    move({sourceId, targetId}) {
+    const lanes = this.lanes;
+    const sourceLane = lanes.filter(lane => lane.notes.includes(sourceId))[0];
+    const targetLane = lanes.filter(lane => lane.notes.includes(targetId))[0];
+    const sourceNoteIndex = sourceLane.notes.indexOf(sourceId);
+    const targetNoteIndex = targetLane.notes.indexOf(targetId);
+
+    if(sourceLane === targetLane) {
+      // move at once to avoid complications
+      sourceLane.notes = update(sourceLane.notes, {
+        $splice: [
+          [sourceNoteIndex, 1],
+          [targetNoteIndex, 0, sourceId]
+        ]
+      });
+    }
+    else {
+      // get rid of the source
+      sourceLane.notes.splice(sourceNoteIndex, 1);
+
+      // and move it to target
+      targetLane.notes.splice(targetNoteIndex, 0, sourceId);
+    }
+
+    this.setState({lanes});
+  }
 }
